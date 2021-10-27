c0ddc91ab45b462255cabd0497df6f2c9513160f
Lorent Elle <lelle@un-a5.kzn.21-school.ru>
2021-10-27 19:07:04 +0300

    additional keys for env

---
+++ Bldins/additional_keys.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bldin_pwd.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lelle <lelle@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/12 14:27:46 by lelle             #+#    #+#             */
+/*   Updated: 2021/10/23 16:52:56 by gvolibea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../ft_minishell.h"
+
+int	add_pwd(t_env *env)
+{
+	char	*pwd;
+	t_env	*t;
+
+	t = env;
+	if (env == NULL)
+		write(1, "env = NULL\n", 11);
+	
+	pwd = malloc(4096 * sizeof(char));
+	if (pwd == NULL)
+		return(msg_mallocfail());
+	getcwd(pwd, 4096);
+	while (t != NULL)
+	{
+		if (ft_strncmp(t->key_vl, "PWD=", 4) == 0 && t->flg == 4)
+		{
+			free(t->key_vl);
+			break ;
+		}
+		t = t->next;
+	}
+		
+	if (t == NULL)
+	{
+		add_new_list(env, 4, -1);
+		t = last_list(env);
+	}
+	t->key_vl = ft_strjoin("PWD=", pwd);
+	if (t->key_vl == NULL)
+		return(msg_mallocfail());
+	free(pwd);
+	return (0);
+}
+
+//additionally HOME is added: if HOME is removed and cd ~ is executed
+//flag = 4;
+int	add_home(t_env *env)
+{
+	char	*home_addit;
+
+	while (env != NULL)
+	{
+		if (ft_strncmp(env->key_vl, "HOME=", 5) == 0)
+		{
+			home_addit = env->key_vl;
+			break ;
+		}
+		env = env->next;
+	}
+	if (env == NULL)
+	{
+		ft_putstr_fd("HOME should be in env as in real bash!\n", STDERR_FILENO);
+		exit (1);
+	}
+	add_new_list(env, 4, -1);
+	(last_list(env))->key_vl = ft_strdup(home_addit);
+	if ((last_list(env))->key_vl == NULL)
+		return (msg_mallocfail());
+	return (0);
+}
+
+int	add_addit_keys(t_env *env)
+{
+	if (add_home(env) || add_pwd(env))
+		return (g_exitcode);
+	return(0);
+}
